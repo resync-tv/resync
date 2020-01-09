fd58724f3678518f1f68a9e7738b7460af709023
vaaski <admin@vaa.ski>
2020-01-09 02:59:58 +0100

    added error tracking

--- src/main.js
+++ src/main.js
@@ -6,6 +6,20 @@ import store from "./store"
 import io from "socket.io-client"
 import VueYoutube from "vue-youtube"
 
+import * as Sentry from "@sentry/browser"
+import * as Integrations from "@sentry/integrations"
+
+Sentry.init({
+  dsn: "https://8bfe86f5c97c45e69725e133ddc95e1d@sentry.io/1875610",
+  integrations: [
+    new Integrations.Vue({ Vue, attachProps: true, logErrors: true }),
+  ],
+})
+
+Sentry.configureScope(function(scope) {
+  scope.setUser({ username: store.state.name || "no name set yet" })
+})
+
 const dev = process.env.NODE_ENV === "development"
 
 const socket = dev
