1277c3266b15b733ce6beb50181126a4fd04ba17
vaaski <admin@vaa.ski>
2021-03-28 22:53:24 +0200

    feat: hide player controls when not hovering

--- src/components/PlayerWrapper.vue
+++ src/components/PlayerWrapper.vue
@@ -2,13 +2,14 @@
 import type { MediaType, MediaVideo } from "$/mediaSource"
 import type { RoomState, VideoMetadata } from "$/room"
 
-import { computed, defineComponent, PropType, ref, toRefs } from "vue"
+import { computed, defineComponent, inject, PropType, ref, toRefs } from "vue"
 import { debounce } from "ts-debounce"
 
 import VideoPlayer from "@/components/VideoPlayer"
 import PlayerControls from "@/components/PlayerControls.vue"
 
 import debug from "debug"
+import Resync from "@/resync"
 const log = debug("resync:playerwrapper")
 
 export default defineComponent({
@@ -29,6 +30,10 @@ export default defineComponent({
   },
   setup(props) {
     const { state } = toRefs(props)
+
+    const resync = inject<Resync>("resync")
+    if (!resync) throw new Error("resync injection failed")
+
     const videoW = ref(16)
     const videoH = ref(9)
     const onMetadata = (metadata: VideoMetadata) => {
@@ -77,25 +82,39 @@ export default defineComponent({
       state,
       onMetadata,
       sizeStyle,
+      paused: resync.paused,
     }
   },
 })
 </script>
 
 <template>
-  <div class="rounded flex overflow-hidden relative light:shadow" :style="sizeStyle">
+  <div
+    class="rounded flex overflow-hidden relative light:shadow"
+    :style="sizeStyle"
+    id="player-wrapper"
+  >
     <VideoPlayer :state="state" @metadata="onMetadata" :style="sizeStyle" />
-    <div class="transition-opacity overlay-gradient">
+    <div class="transition-opacity overlay-gradient" :class="{ active: paused }" id="controls">
       <PlayerControls :state="state" class="pointer-events-auto" />
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .overlay-gradient {
   background: linear-gradient(to top, rgba(0, 0, 0, 0.5), transparent);
 
   @apply flex h-1/4 w-full bottom-0 absolute items-end justify-center;
   @apply pointer-events-none;
+}
+
+#controls {
+  opacity: 0;
+}
+
+#player-wrapper:hover > #controls,
+#controls.active {
+  opacity: 1;
 }
 </style>
