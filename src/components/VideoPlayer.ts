ec6d15042e9d57bca7b8c3bbb30549dce5472351
vaaski <admin@vaa.ski>
2021-05-03 21:07:03 +0200

    perf(front): remove extranous console.log

--- src/components/VideoPlayer.ts
+++ src/components/VideoPlayer.ts
@@ -45,8 +45,6 @@ export default defineComponent({
 
       video.value.volume = resync.muted.value ? 0 : resync.volume.value
 
-      console.log("mounted state", resync.state.value.paused)
-
       if (resync.state.value.paused) {
         autoplay.value = false
         video.value.currentTime = resync.state.value.lastSeekedTo
