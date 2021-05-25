fa6b33c93576a3da2550fa5accc9a7382d8f1e71
vaaski <admin@vaa.ski>
2021-05-25 20:14:36 +0200

    test: prepare backend for unit testing

--- backend/index.ts
+++ backend/index.ts
@@ -1,30 +1,8 @@
 console.log("starting resync")
 import "./sentry"
 
-import { createServer } from "http"
-import { Server } from "socket.io"
-import room from "./room"
-import search from "./search"
+import server from "./server"
 
-import debug from "debug"
-const log = debug("resync:index")
-
-const isDev = process.env.NODE_ENV === "development"
-log("dev", isDev)
-
-const origin = isDev ? "*" : ["https://resync.tv", /\.netlify\.app$/]
-const httpServer = createServer()
-const io = new Server(httpServer, {
-  cors: { origin },
-})
 const port = Number(process.env.BACKEND_PORT || 3020)
 
-io.on("connection", client => {
-  log("client connected", client.id)
-  client.on("disconnect", () => log("client disconnected", client.id))
-})
-
-room(io)
-search(io)
-
-httpServer.listen(port).on("listening", () => console.log(`resync listening on ${port}`))
+server(port).then(() => console.log(`resync listening on ${port}`))
---
+++ backend/server.ts
@@ -0,0 +1,33 @@
+import { createServer } from "http"
+import { Server } from "socket.io"
+import room from "./room"
+import search from "./search"
+
+import debug from "debug"
+const log = debug("resync:server")
+
+const isDev = process.env.NODE_ENV === "development"
+log("dev", isDev)
+
+const origin = isDev ? "*" : ["https://resync.tv", /\.netlify\.app$/]
+
+export default (port: number): Promise<void> => {
+  return new Promise((res, rej) => {
+    {
+      const httpServer = createServer()
+      const io = new Server(httpServer, {
+        cors: { origin },
+      })
+
+      io.on("connection", client => {
+        log("client connected", client.id)
+        client.on("disconnect", () => log("client disconnected", client.id))
+      })
+
+      room(io)
+      search(io)
+
+      httpServer.listen(port).on("listening", res).on("error", rej)
+    }
+  })
+}
