42a716f1ac6522b37924ef289ec9d9b7c8b97dc1
moritzkalwa <kalwa.moritz@gmail.com>
2022-01-08 00:29:02 +0100

    fix: made the regular expression more robust

--- src/timestamp.ts
+++ src/timestamp.ts
@@ -2,7 +2,7 @@ export function getTimestamp(url: string) : number {
     let startFrom = 0
     const timestamp = (new URL(url)).searchParams.get('t')
     if (timestamp) {
-      const match = /((\d*?)m(\d*?)s)|(\d*)/g.exec(timestamp)
+      const match = /^(?:((\d*?)m(\d*?)s)|(\d*?))$/g.exec(timestamp)
       if (match && match[2] && match[3]) {
         startFrom = parseInt(match[2])*60 + parseInt(match[3])
       } else if (match && match[4]) {
@@ -10,4 +10,17 @@ export function getTimestamp(url: string) : number {
       }
     }
     return startFrom
+}
+
+export function hasTimestamp(url: string) : boolean {
+    const timestamp = (new URL(url)).searchParams.get('t')
+    if (timestamp) {
+      const match = /^(?:((\d*?)m(\d*?)s)|(\d*?))$/g.exec(timestamp)
+      if (match && match[2] && match[3]) {
+          return true
+      } else if (match && match[4]) {
+          return true
+      }
+    }
+    return false
 }
