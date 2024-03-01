aed7b233babbb14b802d7b3079c61d2da00e7e8e
Oskar Sommer <oskar.sommer@telekom.de>
2024-03-01 13:03:54 +0100

    nuke

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-console.log("starting resync")
-import "./sentry"
-
-import server from "./server"
-
-let port = Number(process.env.BACKEND_PORT ?? 3020)
-if (process.env.NODE_ENV === "staging") port = Number(process.env.STAGING_PORT ?? 6969)
-
-server(port).then(() => console.log(`resync listening on ${port}`))
